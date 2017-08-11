f5e753aee7a87c1f4a7faaffcadb3dbb3afdc557
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-08-11 12:38:22 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
-
-# import scraperwiki
+import scraperwiki
 # import lxml.html
 #
 # # Read in a page
@@ -22,3 +21,32 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+#counties = ['adair','alfalfa','atoka','beaver','beckham','blaine','bryan','caddo','canadian','carter','cherokee','choctaw','cimarron','coal','comanche','cotton','craig','creek','bristow','drumright','custer','delaware','dewey','ellis','garfield','garvin','grady','grant','greer','harmon','harper','haskell','hughes','jackson','jefferson','johnston','kay','poncacity','kingfisher','kiowa','latimer','leflore','lincoln','logan','love','major','marshall','mayes','mcclain','mccurtain','mcintosh','murray','muskogee','noble','nowata','okfuskee','oklahoma','okmulgee','henryetta','osage','ottawa','payne','pawnee','pittsburg','pontotoc','pottawatomie','pushmataha','rogermills','rogers','seminole','sequoyah','stephens','texas','tillman','wagoner','washington','washita','woods','woodward']
+counties = ['oklahoma','Tulsa','cleveland']
+#next_link = 0
+years = ['2011','2012','2013','2014','2015','2016','2017']
+CrimeSeverity = ['CF','CM']
+
+def CaseEndingNumbers():
+    for x in range(1, 7500):
+        yield '%d' % x
+
+
+
+
+def GetOklahomaStateCases():
+    for county in counties:
+        for CaseEndingNumber in ListOfCaseEndingNumbers:
+            for year in years:
+                for severity in CrimeSeverity:
+                  record = {}
+                  #print 'http://www.oscn.net/dockets/GetCaseInformation.aspx?db=%s&number=%s-%s-%s' % (county, severity, year, CaseEndingNumber)
+                  record['case'] ='http://www.oscn.net/dockets/GetCaseInformation.aspx?db=%s&number=%s-%s-%s' % (county, severity, year, CaseEndingNumber)
+                  print 'Case URL:', record
+                  scraperwiki.sqlite.save(unique_keys=['case'], data=record)
+                
+CaseEndingNumbers()
+ListOfCaseEndingNumbers = list(CaseEndingNumbers())
+GetOklahomaStateCases()
+#scraperwiki.sqlite.save(['case'], record)
